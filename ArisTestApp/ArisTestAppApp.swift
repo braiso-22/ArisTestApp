5e369cf0c7830e355806d66e336e5f1c89476865
Brais <braisfv22@gmail.com>
2025-01-25 00:45:02 +0100

    Initial commit

---
+++ ArisTestApp/ArisTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArisTestAppApp.swift
+//  ArisTestApp
+//
+//  Created by Brais Fernández Vázquez on 25/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ArisTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArisTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArisTestApp
+//
+//  Created by Brais Fernández Vázquez on 25/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
