c2bb915230e63c7c3afe5b175a2e30a2604066a8
Brais <braisfv22@gmail.com>
2025-01-26 20:22:19 +0100

    updated view to show the list of heroes

--- ArisTestApp/SuperHeroFinder/SuperHeroFinderView.swift
+++ ArisTestApp/SuperHeroFinder/SuperHeroFinderView.swift
@@ -7,11 +7,60 @@
 
 import SwiftUI
 
+struct HeroItem: Identifiable {
+    var id: String
+    var name: String
+}
+
 struct SuperHeroFinderView: View {
+    @State private var searchText = ""
+    @State private var searchIsActive = false
+    
+    @State private var isLoading: Bool = false
+    
+    @State private var superHeroes: [HeroItem] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            if(isLoading){
+                ProgressView()
+            }
+            if(!isLoading && superHeroes.isEmpty){
+                Text("Search for an existing super hero")
+            }
+            List(superHeroes){ hero in
+                NavigationLink(destination: {}){
+                    Text(hero.name)
+                }
+            }
+            .navigationTitle("Super hero finder")
+            .searchable(
+                text: $searchText,
+                // this is a bug in swift ui
+                placement: .navigationBarDrawer(displayMode: .always)
+            )
+            .onSubmit(of: .search, runSearch)
+            
+        }
+    }
+    
+    func runSearch(){
+        Task{
+            do{
+                isLoading = true
+                let test = try await SuperHeroApi().getHeroesByQuery(query:searchText)
+                superHeroes = test.results.map{ result in
+                    HeroItem(id: result.id, name: result.name)
+                }
+                isLoading = false
+            } catch {
+                isLoading = false
+                print("error \(error)")
+            }
+        }
     }
 }
+
 
 #Preview {
     SuperHeroFinderView()
